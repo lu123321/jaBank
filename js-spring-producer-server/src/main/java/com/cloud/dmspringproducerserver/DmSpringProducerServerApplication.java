f5be8c066029678542d7ebef666375df28bd03ee
github19950430 <44158178+github19950430@users.noreply.github.com>
2018-11-08 15:43:50 +0800

    Easy code 创建dao service impl controller

--- js-spring-producer-server/src/main/java/com/cloud/dmspringproducerserver/DmSpringProducerServerApplication.java
+++ js-spring-producer-server/src/main/java/com/cloud/dmspringproducerserver/DmSpringProducerServerApplication.java
@@ -1,11 +1,13 @@
 package com.cloud.dmspringproducerserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.cloud.dmspringproducerserver.*")
 public class DmSpringProducerServerApplication {
 
     public static void main(String[] args) {
