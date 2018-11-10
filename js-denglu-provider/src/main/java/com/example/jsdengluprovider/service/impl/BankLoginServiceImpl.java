266518690773426c0135cd671616ddfe5eaef7c4
panghanwen <259242785@qq.com>
2018-11-10 08:44:11 +0800

    test

--- js-denglu-provider/src/main/java/com/example/jsdengluprovider/service/impl/BankLoginServiceImpl.java
+++ js-denglu-provider/src/main/java/com/example/jsdengluprovider/service/impl/BankLoginServiceImpl.java
@@ -1,23 +1,37 @@
 package com.example.jsdengluprovider.service.impl;
 
+import com.example.jsdengluprovider.dao.BankLoginDao;
+import com.example.jsdengluprovider.pojo.BankCard;
 import com.example.jsdengluprovider.service.BankLoginService;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+
 @Service
 public class BankLoginServiceImpl implements BankLoginService {
+    @Resource
+    private BankLoginDao bankLoginDao;
 
-    @Override
-    public String iphoneLogin(String iphone, String webpwd) {
-        return null;
+    public BankLoginDao getBankLoginDao() {
+        return bankLoginDao;
+    }
+
+    public void setBankLoginDao(BankLoginDao bankLoginDao) {
+        this.bankLoginDao = bankLoginDao;
     }
 
     @Override
-    public String cardLogin(String cardnum, String webpwd) {
-        return null;
+    public BankCard phoneLogin(String phone, String webpwd) {
+        return bankLoginDao.phoneLogin(phone,webpwd);
     }
 
     @Override
-    public String usernameLogin(String username, String webpwd) {
-        return null;
+    public BankCard cardLogin(String cardnum, String webpwd) {
+        return bankLoginDao.cardLogin(cardnum,webpwd);
+    }
+
+    @Override
+    public BankCard usernameLogin(String username, String webpwd) {
+        return bankLoginDao.usernameLogin(username,webpwd);
     }
 }
