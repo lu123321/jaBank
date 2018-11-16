0c232f5175ee8f098f4af636898ea97ed653c4b5
panghanwen <259242785@qq.com>
2018-11-16 20:54:36 +0800

    test

--- js-denglu-provider/src/main/java/com/example/jsdengluprovider/dao/AppointmentSelectDao.java
+++ js-denglu-provider/src/main/java/com/example/jsdengluprovider/dao/AppointmentSelectDao.java
@@ -2,8 +2,10 @@ package com.example.jsdengluprovider.dao;
 
 
 import com.example.jsdengluprovider.pojo.Appointment;
+import org.springframework.stereotype.Component;
 
 //预约查询
+@Component
 public interface AppointmentSelectDao {
     //个人预约信息查询
     Appointment appoinSelect();
