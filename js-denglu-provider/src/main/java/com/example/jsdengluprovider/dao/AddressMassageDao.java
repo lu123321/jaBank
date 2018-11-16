b2d979fb01a14a7544a0f816e0442f0b556a71ea
panghanwen <259242785@qq.com>
2018-11-16 20:54:25 +0800

    test

--- js-denglu-provider/src/main/java/com/example/jsdengluprovider/dao/AddressMassageDao.java
+++ js-denglu-provider/src/main/java/com/example/jsdengluprovider/dao/AddressMassageDao.java
@@ -1,18 +1,21 @@
 package com.example.jsdengluprovider.dao;
 
 import com.example.jsdengluprovider.pojo.Address;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface AddressMassageDao {
     //常用地址查询
     @Select("select addressid,address,phone,addresstype,addressstate,phonestate from address where userid=#{userid}")
-    Address addSelect(Integer userid);
+    Address addSelect(@Param("userid") Integer userid);
     //常用地址删除
-    @Update("update address set address=#{address},phone=#{phone},addresstype=#{addresstype},addressstate=#{addressstate},phonestate=#{phonestate} where addressid=#{addressid}")
+    @Delete("delete from address where addressid=#{addressid}")
     int delAddress(Address address);
     //常用地址修改
+    @Update("update address set address=#{address},phone=#{phone},addresstype=#{addresstype},addressstate=#{addressstate},phonestate=#{phonestate} where addressid=#{addressid}")
     int updateAddress(Address address);
     //常用地址增加
+    @Insert("insert into address(addressid,address,phone,userid,addresstype,addressstate,phonestate) values (0,#{address},#{phone},#{userid},#{addresstype},#{addressstate},#{phonestate})")
     int addAddress(Address address);
 }
