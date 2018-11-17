5d5e9327138a6b6cc4d36fba1c4f03027a2b61b6
git <191762443@qq.com>
2018-11-17 10:49:45 +0800

    项目更新

--- js_deposit_provider/src/test/java/com/example/js_deposit_provider/JsDepositProviderApplicationTests.java
+++ js_deposit_provider/src/test/java/com/example/js_deposit_provider/JsDepositProviderApplicationTests.java
@@ -1,27 +1,32 @@
 package com.example.js_deposit_provider;
 
+import com.example.js_deposit_provider.dao.DepositFixationDao;
+import com.example.js_deposit_provider.entity.DepositFixation;
 import com.example.js_deposit_provider.util.DateUtil;
+import com.example.js_deposit_provider.util.IndustrySMS;
 import com.example.js_deposit_provider.util.OrderNumberUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
 import java.text.DecimalFormat;
 import java.util.Date;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class JsDepositProviderApplicationTests {
-
+    @Resource
+    private  DepositFixationDao depositFixationDao;
     @Test
     public void contextLoads() {
-        Date date1 = DateUtil.SchangeD("2018-11-11");
         String nowDate = DateUtil.getNowDate();
-        Date date = DateUtil.SchangeD(nowDate);
-        boolean sameDate = DateUtil.isSameDate(date1, date);
-        System.out.println(sameDate);
-
+        List<DepositFixation> all = depositFixationDao.getAll(nowDate);
+        for (DepositFixation d: all) {
+            System.out.println(d.getDepositFixationid());
+        }
     }
 
 }
